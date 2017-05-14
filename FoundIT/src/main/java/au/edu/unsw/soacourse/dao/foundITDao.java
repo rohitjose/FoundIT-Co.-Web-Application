dc7fd53ed046056c0685357bdf2ad59239fc2354
rohitjose <rohitjose@gmail.com>
2017-05-15 02:06:17 +1000

    Code Update - Signup Dao methods

---
+++ FoundIT/src/main/java/au/edu/unsw/soacourse/dao/foundITDao.java
@@ -0,0 +1,139 @@
+package au.edu.unsw.soacourse.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+
+import au.edu.unsw.soacourse.dto.User;
+
+public class foundITDao {
+	Connection c;
+	String sql_connector = "','";
+
+	// Initialize the connection
+	public foundITDao() {
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:" + this.getClass().getResource("/").getPath() + "foundIT");
+			c.setAutoCommit(false);
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+		System.out.println("Opened database successfully");
+	}
+
+	/* INSERT OPERATIONS FOR THE TABLES */
+
+	public void executeSQL(String sql) throws SQLException {
+		Statement stmt = c.createStatement();
+
+		System.out.println(sql);
+
+		stmt.executeUpdate(sql);
+		stmt.close();
+		c.commit();
+	}
+
+	/* SELECT OPERATIONS FOR THE TABLES */
+
+	public int retrieveValue(String query) {
+
+		int count = 0;
+
+		try {
+
+			// STEP 4: Execute a query
+			Statement stmt = c.createStatement();
+
+			String sql = query;
+			ResultSet rs = stmt.executeQuery(sql);
+
+			while (rs.next()) {
+				count = rs.getInt("value");
+			}
+
+		} catch (SQLException se) {
+			// Handle errors for JDBC
+			se.printStackTrace();
+		}
+		return count;
+
+	}
+
+	public String getValue(HashMap<String, String> map, String key) {
+		if (map.containsKey(key)) {
+			return map.get(key);
+		} else {
+			return "";
+		}
+	}
+
+	/* USER TABLE */
+	public void signUpUser(HashMap<String, String> queryParams) {
+		// Insert into the user table
+		String user_sql = "INSERT INTO user (username,email,password,role)  VALUES('" + queryParams.get("username")
+				+ sql_connector + queryParams.get("email") + sql_connector + queryParams.get("password") + sql_connector
+				+ queryParams.get("role") + "')";
+		String user_id_sql = "SELECT id AS value FROM user ORDER BY id DESC LIMIT 1";
+		int user_id = 0;
+		
+		String role = queryParams.get("role");
+
+		try {
+			executeSQL(user_sql);
+			user_id = retrieveValue(user_id_sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		String profile_sql = "INSERT INTO profile(user_id,first_name,last_name,phone,site,street_address,city,zip,state,headline,position,company,summary,business_domain,experience,education,skills) VALUES("
+				+ user_id + ", '" + getValue(queryParams, "first_name") + sql_connector
+				+ getValue(queryParams, "last_name") + sql_connector + getValue(queryParams, "phone") + sql_connector
+				+ getValue(queryParams, "site") + sql_connector + getValue(queryParams, "street_address")
+				+ sql_connector + getValue(queryParams, "city") + sql_connector + getValue(queryParams, "zip")
+				+ sql_connector + getValue(queryParams, "state") + sql_connector + getValue(queryParams, "headline")
+				+ sql_connector + getValue(queryParams, "position") + sql_connector + getValue(queryParams, "company")
+				+ sql_connector + getValue(queryParams, "business_domain") + sql_connector
+				+ getValue(queryParams, "experience") + sql_connector + getValue(queryParams, "education")
+				+ sql_connector + getValue(queryParams, "skills") + "')";
+		
+		try {
+			executeSQL(profile_sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	// Login Support function
+	public User login(String username, String password) throws SQLException{
+		String sql = "SELECT * FROM user WHERE username='"+username+"'";
+		User user = new User();
+		String auth_pass = new String();
+		
+		Statement stmt = c.createStatement();
+		ResultSet rs = stmt.executeQuery(sql);
+		
+		while (rs.next()) {
+			user.setUser_id(rs.getInt("id"));
+			user.setEmail(rs.getString("username"));
+			auth_pass = rs.getString("password");
+			user.setRole(rs.getString("role"));
+		}
+		rs.close();
+		stmt.close();
+		
+		if(auth_pass.equals(password)){
+			return user;
+		}else{
+			return null;
+		}
+	}
+
+}
---
+++ FoundIT/src/main/java/au/edu/unsw/soacourse/dto/User.java
@@ -0,0 +1,59 @@
+package au.edu.unsw.soacourse.dto;
+
+public class User {
+	int user_id;
+	String username;
+	String email;
+	String role;
+	/**
+	 * @return the user_id
+	 */
+	public int getUser_id() {
+		return user_id;
+	}
+	/**
+	 * @param user_id the user_id to set
+	 */
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	/**
+	 * @return the role
+	 */
+	public String getRole() {
+		return role;
+	}
+	/**
+	 * @param role the role to set
+	 */
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
+
+}
