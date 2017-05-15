f36da594d2fa2d0819db051ee6225d27fd483b17
alihk <alihaiderkhan@Alis-MacBook-Pro.local>
2017-05-16 03:54:28 +1000

    jobs added

---
+++ jobs/FoundIT/src/main/java/com/soacourse/ass2/foundIt/HelloWorld.java
@@ -0,0 +1,29 @@
+package com.soacourse.ass2.foundIt;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+@Path("/hello")
+public class HelloWorld {
+
+    @GET
+    @Path("/echo/{text}")
+    @Produces("text/plain")
+    public String ping(@PathParam("text") String input) {
+        return input;
+    }
+
+    @POST
+    @Produces("application/json")
+    @Consumes("application/json")
+    @Path("/jsonBean")
+    public Response modifyJson(JsonBean input) {
+        input.setVal2(input.getVal1());
+        return Response.ok().entity(input).build();
+    }
+}
+
